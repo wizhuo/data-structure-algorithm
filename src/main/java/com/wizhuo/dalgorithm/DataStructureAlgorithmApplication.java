b7579e486103a6ee4924f376ef595fcecbf134a5
willJo <share_lz@163.com>
2021-09-29 10:10:38 +0800

    init

---
+++ src/main/java/com/wizhuo/dalgorithm/DataStructureAlgorithmApplication.java
@@ -0,0 +1,9 @@
+package com.wizhuo.dalgorithm;
+
+
+public class DataStructureAlgorithmApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
---
+++ src/test/java/com/wizhuo/dalgorithm/DataStructureAlgorithmApplicationTests.java
@@ -0,0 +1,15 @@
+package com.wizhuo.dalgorithm;
+
+
+import org.junit.Test;
+
+public class DataStructureAlgorithmApplicationTests {
+
+	@Test
+	public void test(){
+
+		System.out.println("hello");
+	}
+
+
+}
